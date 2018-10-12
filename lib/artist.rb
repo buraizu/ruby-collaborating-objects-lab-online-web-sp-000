3ae91472ea8d2da3e3156108cb6a1fbbcb7dec10
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-12 18:43:58 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,7 +26,7 @@ class Artist
     if @@all.length > 0
       @@all.each do |artist|
         if artist.name == name
-          puts artist
+          puts artist.name
           binding.pry
           return artist
         end
@@ -46,27 +46,27 @@ class Artist
 end
 
 # class Artist
-# 
+#
 #   attr_accessor :name, :songs
 #   @@all = []
-# 
+#
 #   def initialize(name)
 #     @name = name
 #     @songs = []
 #   end
-# 
+#
 #   def add_song(song)
 #     @songs << song
 #   end
-# 
+#
 #   def save
 #     @@all << self
 #   end
-# 
+#
 #   def self.all
 #     @@all
 #   end
-# 
+#
 #   def self.find_or_create_by_name(artist)
 #     unless @@all == []
 #       @@all.each do |a|
@@ -75,14 +75,14 @@ end
 #         end
 #       end
 #     end
-# 
+#
 #       new_artist = Artist.new(artist)
 #       new_artist.save
 #       new_artist
 #   end
-# 
+#
 #   def print_songs
 #     self.songs.each {|s| puts s.name}
 #   end
-# 
+#
 # end
