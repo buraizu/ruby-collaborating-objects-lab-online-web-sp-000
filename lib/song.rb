86b3a966ed1f4802410856ba05fc04eaa4f88106
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-12 18:43:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,13 @@
 # require 'pry'
-# 
+#
 # class Song
-# 
+#
 #   attr_accessor :name, :artist
-# 
+#
 #   def initialize(name)
 #     @name = name
 #   end
-# 
+#
 #   def self.new_by_filename(filename)
 #     data = filename.split(" - ")
 #     new_song_artist = data[0]
@@ -17,13 +17,13 @@
 #     puts new_song
 #     new_song
 #   end
-# 
+#
 #   def artist_name=(name)
 #     self.artist = Artist.find_or_create_by_name(name)
 #     artist.add_song(self)
 #   end
-# 
-# 
+#
+#
 # end
 
 class Song
