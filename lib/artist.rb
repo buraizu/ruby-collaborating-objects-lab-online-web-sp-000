216901d689842d073cfa2a9e0b6a2aae4191d4ff
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-12 19:03:52 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -30,7 +30,6 @@ class Artist
           end
         end
       end
-
         new_artist = Artist.new(artist)
         new_artist.save
         new_artist
@@ -44,44 +43,3 @@ class Artist
 
 end
 
-# class Artist
-#
-#   attr_accessor :name, :songs
-#   @@all = []
-#
-#   def initialize(name)
-#     @name = name
-#     @songs = []
-#   end
-#
-#   def add_song(song)
-#     @songs << song
-#   end
-#
-#   def save
-#     @@all << self
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def self.find_or_create_by_name(artist)
-#     unless @@all == []
-#       @@all.each do |a|
-#         if a.name == artist
-#           return a
-#         end
-#       end
-#     end
-#
-#       new_artist = Artist.new(artist)
-#       new_artist.save
-#       new_artist
-#   end
-#
-#   def print_songs
-#     self.songs.each {|s| puts s.name}
-#   end
-#
-# end
