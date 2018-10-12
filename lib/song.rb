9cc19ed785c4868022707884d7ecc9ec600ba415
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-12 18:24:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class Song
+
   attr_accessor :name, :artist
 
   def initialize(name)
@@ -8,13 +9,20 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    parsed = filename.split(" - ")
-    artist_name = parsed[0]
-    title = parsed[1]
-    new_song = self.new(title)
-    new_artist = Artist.find_or_create_by_name(artist_name)
-    new_song.artist = new_artist
-    new_artist.add_song(new_song)
+    data = filename.split(" - ")
+    new_song_title = data[1]
+    new_song = self.new(new_song_title)
+    new_song.artist_name = data[0]
+    puts new_song
+
     new_song
   end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    puts self.artist
+    binding.pry
+  end
+
+
 end
